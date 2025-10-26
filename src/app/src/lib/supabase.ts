f73480a0bed88527fde867652546c2a9679ccc17
vinsviaggi-hub <vinsviaggi@gmail.com>
2025-10-26 14:13:10 +0100

    Update supabase.ts

--- src/app/src/lib/supabase.ts
+++ src/app/src/lib/supabase.ts
@@ -1,6 +1,12 @@
-import { createClient } from '@supabase/supabase-js'
+// src/lib/supabase.ts
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true,
+    },
+  }
+);
