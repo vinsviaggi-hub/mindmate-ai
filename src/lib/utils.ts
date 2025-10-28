0c0d541b1db575d4048f985a25e979888824543a
vinsviaggi-hub <vinsviaggi@gmail.com>
2025-10-28 23:26:54 +0100

    Delete src/lib/utils.ts

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-export function getLS<T>(key: string, defaultValue: T): T {
-  if (typeof window === "undefined") return defaultValue;
-  try {
-    const value = localStorage.getItem(key);
-    return value ? (JSON.parse(value) as T) : defaultValue;
-  } catch {
-    return defaultValue;
-  }
-}
