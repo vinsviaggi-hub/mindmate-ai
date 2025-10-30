daaac057818af21017bb6d2dc9cdecd6eb58da11
vinsviaggi-hub <vinsviaggi@gmail.com>
2025-10-30 11:47:16 +0100

    Update UserBar.tsx

--- src/app/UserBar.tsx
+++ src/app/UserBar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { supabaseBrowser } from "@/lib/supabase/client";
-
+import supabase from "@/lib/supabase";
 type Stats = {
   current_streak: number | null;
   longest_streak: number | null;
