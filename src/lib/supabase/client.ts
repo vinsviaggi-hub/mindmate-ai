5b84764b3ec2a60ddcc41065c8bd07324f0cd3cf
vinsviaggi-hub <vinsviaggi@gmail.com>
2025-10-30 11:49:07 +0100

    Delete src/lib/supabase directory

--- src/lib/supabase/client.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/supabase/client.ts
-import { createClient } from "@supabase/supabase-js";
-
-export function supabaseBrowser() {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
